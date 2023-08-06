d0d730d82f4a92e6d98a9140a05dc4fd3e092fd7
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2023-08-07 08:47:41 +0900

    create home route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
-  return;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Will be home</h1>;
+}
+
+export default Home;
