b730e595700e0771d413a334b867fee36b5254d5
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2023-08-08 13:49:51 +0900

    render random word

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
+import wordsJson from "../words.json";
+import styles from "./Game.module.css";
 
 function Game() {
+  const [words, setWords] = useState([]);
+  const [round, setRound] = useState([1]);
+  const { chinese, pureK } = wordsJson;
+  const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+  const changeWords = () => {
+    const newChinese = shuffle(chinese);
+    const newPureK = shuffle(pureK);
+    const newWords = shuffle([
+      newChinese.pop(),
+      newChinese.pop(),
+      newChinese.pop(),
+      newPureK.pop(),
+    ]);
+    setWords(newWords);
+  };
+  useEffect(() => {
+    if (chinese.length > 0 && pureK.length > 0) changeWords();
+  }, [round]);
   return (
     <div>
       <h1>Will be game</h1>
-      <Card text="천지" chinese="天[하늘 천] 地[땅 지]" />
+      {words ? (
+        <div className={styles.cards}>
+          {words.map((item) => {
+            return (
+              <Card
+                key={item.word}
+                text={item.word}
+                chinese={item.chChar ? item.chChar : null}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
