69b71280e822d1a25d1e486382a41f9fb3b76d7f
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2023-08-08 16:22:39 +0900

    card click event

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,19 @@
 import styles from "./Card.module.css";
 import PropTypes from "prop-types";
 
-function Card({ text, chinese }) {
+function Card({ text, chinese, onClick, isClicked }) {
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={onClick}>
       <h1 className={styles.card__word}>{text}</h1>
-      <span className={styles.card__chinese}>{chinese}</span>
+      <span
+        className={
+          isClicked
+            ? styles.card__chinese
+            : `${styles.card__chinese} ${styles.hide}`
+        }
+      >
+        {chinese}
+      </span>
     </div>
   );
 }
@@ -13,6 +21,7 @@ function Card({ text, chinese }) {
 Card.propTypes = {
   text: PropTypes.string.isRequired,
   chinese: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Card;
--- src/routes/Game.js
+++ src/routes/Game.js
@@ -5,26 +5,61 @@ import styles from "./Game.module.css";
 
 function Game() {
   const [words, setWords] = useState([]);
-  const [round, setRound] = useState([1]);
+  const [round, setRound] = useState(1);
+  const [clicked, setClicked] = useState(false);
+
   const { chinese, pureK } = wordsJson;
   const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
   const changeWords = () => {
-    const newChinese = shuffle(chinese);
-    const newPureK = shuffle(pureK);
-    const newWords = shuffle([
-      newChinese.pop(),
-      newChinese.pop(),
-      newChinese.pop(),
-      newPureK.pop(),
-    ]);
-    setWords(newWords);
+    if (chinese.length > 0 && pureK.length > 0) {
+      const newChinese = shuffle(chinese);
+      const newPureK = shuffle(pureK);
+      const newWords = shuffle([
+        newChinese.pop(),
+        newChinese.pop(),
+        newChinese.pop(),
+        newPureK.pop(),
+      ]);
+      setWords(newWords);
+    }
   };
+
   useEffect(() => {
-    if (chinese.length > 0 && pureK.length > 0) changeWords();
-  }, [round]);
+    changeWords();
+  }, []);
+
+  const roundUp = () => {
+    setRound((prev) => prev + 1);
+  };
+  const roundDown = () => {
+    setRound((prev) => prev - 1);
+  };
+
+  const clickChange = () => setClicked((prev) => !prev);
+  const chineseOnClick = (event) => {
+    if (!clicked) {
+      clickChange();
+      setTimeout(clickChange, 3000);
+      setTimeout(changeWords, 3000);
+      if (round > 1) {
+        setTimeout(roundDown, 3000);
+      }
+    }
+  };
+  const pureKOnClick = () => {
+    if (!clicked) {
+      clickChange();
+      setTimeout(clickChange, 3000);
+      setTimeout(changeWords, 3000);
+      if (round < 5) {
+        setTimeout(roundUp, 3000);
+      }
+    }
+  };
+
   return (
     <div>
-      <h1>Will be game</h1>
+      <h2>Round: {round}</h2>
       {words ? (
         <div className={styles.cards}>
           {words.map((item) => {
@@ -33,6 +68,8 @@ function Game() {
                 key={item.word}
                 text={item.word}
                 chinese={item.chChar ? item.chChar : null}
+                onClick={item.chChar ? chineseOnClick : pureKOnClick}
+                isClicked={clicked}
               />
             );
           })}
